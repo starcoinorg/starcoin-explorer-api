734309cf41150b92a9507ababf50dfc3ae90bc8e
wk3368 <wk3368@163.com>
2020-12-26 14:30:27 +0800

    set Access-Control-Allow-Origin

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	_ "starcoin-api/routers"
 
 	beego "github.com/beego/beego/v2/server/web"
+	"github.com/beego/beego/v2/server/web/filter/cors"
 )
 
 func init() {
@@ -16,5 +17,14 @@ func main() {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
+
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"*"},
+		AllowHeaders:     []string{"*"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+
 	beego.Run()
 }
